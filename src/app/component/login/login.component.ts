191f3c066bd21458c034aa9c5dd8680b06172795
baranerf <erfani.baran@gmail.com>
2023-02-04 04:41:37 -0600

    Added formGroups

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  public formGroup: FormGroup;
+  constructor( private formBuilder: FormBuilder)
+  {}
+
+  ngOnInit() {
+    this.generateFormGroup();
+  }
+
+  generateFormGroup() {
+    this.formGroup = this.formBuilder.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required]]
+    });
+  }
+  onSubmit() {
+    if (this.formGroup.valid) {
+      const email = this.formGroup.get("email")?.value;
+      const password = this.formGroup.get("password")?.value;
+      // send the email and passowrd to auth
+      // if (email && password) {
+      //   this.authService.login(email, password);
+      // }
+    }
+  }
+
 
 }
--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+
+  public formGroup: FormGroup
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+  }
+  ngOnInit() {
+    this.generateFormGroup();
+  }
+  generateFormGroup() {
+    this.formGroup = this.formBuilder.group({
+      firstName: ["", [Validators.required]],
+      lastName: ["", [Validators.required]],
+      email: ["", [Validators.required]],
+      password: ["", [Validators.required]],
+      confirmPassword: ["", [Validators.required]]
+    });
+  }
+  onSubmit() {
+    const first_name = this.formGroup.get("firstName")?.value;
+    const last_name = this.formGroup.get("lastName")?.value;
+    const email = this.formGroup.get("email")?.value;
+    const password = this.formGroup.get("password")?.value;
+    const confirm_password = this.formGroup.get("confirmPassword")?.value;
+
+    // TODO: validations
+
+    // const newUser: NewUser = {
+    //   first_name: first_name,
+    //   last_name: last_name,
+    //   email: email,
+    //   password: password,
+    // }
+
+    // const response = this.authService.register(newUser);
+
+  }
 
 }
