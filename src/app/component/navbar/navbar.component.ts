d1910bf348ae2e2167ef244d1bda235dd870b5d4
Om Dalwadi <88398870+OmDalwadi@users.noreply.github.com>
2023-02-04 11:53:28 -0600

    Login toggles based on the user is logged in or not..

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import { JwtInterceptor } from '../../interceptors/jwt.interceptor';
+import {AuthService} from "../../services/auth.service";
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
 
+export class NavbarComponent implements OnInit {
+  isLoggedIn = false;
+
+  constructor(
+    private authService: AuthService,
+  ) {
+  }
+
+  ngOnInit() {
+    this.isLoggedIn = this.authService.isAuthenticated();
+  }
 }
