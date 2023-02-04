da4cb2efcdb80425343daff4eb323c4621626443
baranerf <erfani.baran@gmail.com>
2023-02-03 23:30:54 -0600

    Added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/component/**/*.{html,ts}",
+    "./projects/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
